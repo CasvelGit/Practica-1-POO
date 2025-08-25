4493515a85a83020fa34490a0a55f19eb09e9fbe
CasvelGit <casvel1234@gmail.com>
2025-08-25 16:10:35 -0600

    Update Practica1.java
    Ahora el collatz imprime la cadena de la conjetura

--- Practica1.java
+++ Practica1.java
@@ -67,7 +67,14 @@ public class Practica1{
     public static int collatz(int n)
     {   
         //Este lo hice de puro mamón porque el profe dijo "Sorprendeme"
-        //No sé si debe solo de llegar a la conjetura o darte cuantos pasos, pero allá se vé
+        if(n==1)
+        {
+            System.out.print(n);
+        }
+        else 
+        {
+            System.out.print(n+"->");
+        }
         return n==1?1:(n%2==0?collatz(n/2):collatz(3*n+1));
     }
 }
